29efbff14e7f1817ee6e7d721840cc15ee5a205e
Arthur Felipe <seu_email>
2026-06-29 22:20:21 -0300

    Primeira versão do sistema de login

---
+++ main.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, request, redirect, url_for
+from db import db
+from models import Usuario
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+import hashlib
+
+app = Flask(__name__)
+app.secret_key = "Arthur"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
+db.init_app(app)
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = "login"
+
+def hash(txt):
+    hash_obj = hashlib.sha256(txt.encode('utf-8'))
+    return hash_obj.hexdigest()
+
+@lm.user_loader
+def user_loader(id):
+    usuario = db.session.query(Usuario).filter_by(id=int(id)).first()
+    return usuario
+
+
+@app.route("/")
+
+def home():
+    return render_template("home.html")
+
+
+
+
+@app.route("/registrar", methods=["GET", "POST"])
+def registrar():
+    if request.method == "GET":
+        return render_template("registrar.html")
+
+    nome = request.form["nomeForm"]
+    senha = request.form["senhaForm"]
+
+    novo_usuario = Usuario(nome=nome, senha=hash(senha))
+
+    db.session.add(novo_usuario)
+    db.session.commit()
+
+    login_user(novo_usuario)
+
+    return redirect(url_for("home"))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    nome = request.form["nomeForm"]
+    senha = request.form["senhaForm"]
+
+    user = db.session.query(Usuario).filter_by(nome=nome, senha=hash(senha)).first()
+
+    if not user:
+        return "Nome ou senha incorretos."
+
+    login_user(user)
+    return redirect(url_for("home"))
+
+
+
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
